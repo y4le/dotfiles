9159c491acce91a5a58358488b91dd6db947515b
Yale Thomas <lordchair@gmail.com>
2017-12-10 00:32:06 -0800

    *simplify*

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 
+brew install tmux
 brew install zsh
+brew install antigen
 brew install vim
 brew install neovim
 brew install fzf
 brew install ranger
 brew install fpp
-
-brew install antigen
 
 rm -f ~/.vimrc
 ln -s `pwd`/vimrc ~/.vimrc
@@ -14,4 +14,6 @@ ln -s `pwd`/vimrc ~/.vimrc
 rm -f ~/.zshrc
 ln -s `pwd`/zshrc ~/.zshrc
 
+rm -f ~/.tmux.conf
+ln -s `pwd`/tmux.conf ~/.tmux.conf
 
