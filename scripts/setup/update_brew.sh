189b1122d796662a33db09a6b587d21102ec7a26
Yale Thomas <lordchair@gmail.com>
2018-08-30 16:18:43 -0700

    Describe and catagorize brew packages

--- scripts/setup/update_brew.sh
+++ scripts/setup/update_brew.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-brew install tmux
-brew install zsh
-brew install antigen
-brew install vim
-brew install neovim
-brew install fzf
-brew install ranger
-brew install fpp
+core_packages=(
+  tmux    # terminal multiplexer
+  zsh     # e.g. `git status | fpp` -> pick files -> `c` -> `git checkout master`bash replacement
+  antigen # zsh plugin management
+)
+
+editor_packages=(
+  vim # vim 4 life
+  neovim
+)
+
+qol_packages=(
+  fzf # fuzzy finder - uses ripgrep to search - used by vim plugin
+  ripgrep # rg - better cli search - used by fzf
+  ranger # cli file system browser - start with ^k
+  fpp # facebook path picker - pipe in txt, pick files, do thing
+  fd # better `find`
+  jq # json cli
+  bat # better `cat`
+  htop # better `top`
+  ncdu # better `du`
+  tldr # better `man`
+  prettyping # better `ping`
+  diff-so-fancy # better `diff`
+)
+
+packages=(${core_packages[@]} ${editor_packages[@]} ${qol_packages[@]})
+
+brew install ${packages[@]}
+brew upgrade ${packages[@]}
+
