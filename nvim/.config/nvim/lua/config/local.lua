d5619527799a187b3e7f66fada1301afdfd4d7e6
Yale Thomas <lordchair@gmail.com>
2026-05-20 20:37:51 -0400

    Support vim.loop fallback in local nvim config

--- nvim/.config/nvim/lua/config/local.lua
+++ nvim/.config/nvim/lua/config/local.lua
@@ -1,5 +1,6 @@
 local local_init = vim.fn.stdpath("config") .. "/lua/local/init.lua"
+local uv = vim.uv or vim.loop
 
-if vim.uv.fs_stat(local_init) then
+if uv and uv.fs_stat(local_init) then
   dofile(local_init)
 end
